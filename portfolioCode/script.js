ef377cb8b8f965275060a2423df9caa64fe232de
Jorge-Sanchezz <jsanchezencinas0804@gmail.com>
2024-07-04 01:09:47 -0600

    Responsive feature added

--- portfolioCode/script.js
+++ portfolioCode/script.js
@@ -4,3 +4,12 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
